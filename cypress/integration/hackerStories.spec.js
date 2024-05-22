2d4b4b0c7e5fd18e9c2fa42eef799fe8cd39a72e
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2024-05-21 23:45:27 -0300

    Add test failure server and network

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -71,14 +71,6 @@ describe('Hacker Stories', () => {
       it('orders by points', () => { })
     })
 
-    // Hrm, how would I simulate such errors?
-    // Since I still don't know, the tests are being skipped.
-    // TODO: Find a way to test them out.
-    context.skip('Errors', () => {
-      it('shows "Something went wrong ..." in case of a server error', () => { })
-
-      it('shows "Something went wrong ..." in case of a network error', () => { })
-    })
   })
 
   context('Search', () => {
@@ -175,3 +167,23 @@ describe('Hacker Stories', () => {
     })
   })
 })
+
+context.only('Errors', () => {
+      it('shows "Something went wrong ..." in case of a server error', () => {
+        cy.intercept(
+          'GET',
+          '**/search**',
+          { statusCode: 500}
+        ).as('getServerFailure')
+
+        cy.visit('/')
+        cy.wait('@getServerFailure')
+
+        cy.get('p:contains(Something went wrong ...)')
+          .should('be.visible')
+       })
+
+      it('shows "Something went wrong ..." in case of a network error', () => {
+
+       })
+    })
